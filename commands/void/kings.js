6b259cea0a92dac6cf93efe4f54d8888dd3a51ad
Salty Panda <zjadaczwatrob@gmail.com>
2023-05-18 01:29:55 +0200

    add logging to dev server

--- commands/void/kings.js
+++ commands/void/kings.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const {guildId, logChannelId} = require('../..config.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,129 +13,142 @@ module.exports = {
 
 		const role = interaction.guild.roles.cache.find(role => role.id === '553347167583076372'); // Replace with the exact name of the role
 		if (role) {
-console.log(role.name);
+			//console.log(role.name);
 			const membersWithRole = role.members.filter(member => member.presence && member.presence.status);
 			const onlineCount = membersWithRole.size;
-console.log(membersWithRole.map(a => a.user.username));
-console.log(membersWithRole.map(a => a.presence? a.presence.status : "not active"));
-			if (onlineCount >= 1) {
-				let response;
+			//console.log(membersWithRole.map(a => a.user.username));
+			//console.log(membersWithRole.map(a => a.presence ? a.presence.status : "not active"));
 
-				switch (onlineCount) {
-					case 1:
-						response =
-							"There is only one king of undead online. Panda is sad.\n" +
-							"Remember, I'm watching you";
-						break;
-					case 2:
-						response =
-							"There two kings of undead online. Maybe they can try to breed more of them?\n" +
-							"Spoiler: Maybe sometime I'll write custom messages based on who's online \n" +
-							"and in place of the message upward you'll see encouragement for Cow to breed resident batcatgirl";
-						break;
-					case 3:
-						response = "Three kigns. PLACEHOLDER";
-						break;
-					case 4:
-						response = "I AM EVERWATCHING PANDA. PRAISE ME!\n" +
-							"FOUR KINGS OF THE UNDEAD ARE NOW ON!\n" +
-							"BY RIGHT OF THE GREAT PANDA!\n" +
-							"(and that old man too I guess)\n" +
-							"IT'S TIME TO COMMENCE OPERATION:\n" +
-							"PANDEMONIUM!\n" +
-							"THE ELECTRIC BOOGALOO, FOUR!\n" +
-							"RETURN OF THE BOOGALOO";
-						break;
-					case 5:
-						response = "I AM EVERWATCHING PANDA. PRAISE ME!\n" +
-							"FIVE KINGS OF THE UNDEAD ARE NOW ON!\n" +
-							"BY RIGHT OF THE GREAT PANDA!\n" +
-							"(and that old man too I guess)\n" +
-							"IT'S TIME TO COMMENCE OPERATION:\n" +
-							"UNDEAD RISING! FOR THE FIFTH TIME!!!\n" +
-							"THE ELECTRIC BOOGALOO, FIIIIIVE!\n" +
-							"RETURN OF THE BOOGALOO!\n" +
-							"OOGGY BOOGY'S BOOGALOO!\n" +
-							"THE BOOGALOO SPECIAL!\n" +
-							"THE NEXT BOOGALOO!!!!\n" +
-							"LURKERS UNITE FOR TEH BOOGALOO!!!!";
-						break;
-					case 6:
-						response = "I AM EVERWATCHING PANDA. PRAISE ME!\n" +
-							"SIX FUCKING KINGS OF THE UNDEAD;\n" +
-							"BY RIGHT OF THE GREAT PANDA!!!!!\n" +
-							"(and that old man too I guess)\n" +
-							"IT'S TIME TO COMMENCE OPERATION SIX SIX SIX!!!!!!!:\n" +
-							"UNDEAD RISING, LET'S TAKE OVER THIS FUCKING WOOOOORLD!\n" +
-							"THE ELECTRIC BOOGALOO, SIXTY NINE!!!\n" +
-							"NOICE!\n" +
-							"RETURN OF THE BOOGALOO\n" +
-							"ALWAYS THE BOOGALOO!!!!\n" +
-							"BOOGALOO AND FOREVER!\n" +
-							"FOREVER AND EVER BOOGALOO!!!!!!!\n" +
-							"IF YOU HAVEN'T SEEN THE BOOGALOO YOU HAVE NOW, BOOGALOO!\n" +
-							"OH SHIT, IT'S A MOTHER FUCKIN CRUSADE IN THIS BITCH!!!\n" +
-							"RAVE TIME!\n" +
-							"RAVE WITH THE DEAD!\n" +
-							"DEAD RAVE PARTY! BOOOGALOO RAVE!!!!\n" +
-							"J)#H%R_(Q#HRN_(Q#YVRN_Q(#YR0aw=038rya!!!!";
-						break;
-					case 7:
-						response = "I AM EVERWATCHING PANDA. PRAISE ME!\n" +
-							"SEVEN FUCKING SEVEN SEVEN KINGS OF THE UNDEAD;\n" +
-							"BY RIGHT OF THE GREAT PANDA!!!!!\n" +
-							"(and that old man too I guess)\n" +
-							"IT'S TIME TO COMMENCE OPERATION GOD NUMBERS OF SEVEN AND SEVEN!!!!!!!:\n" +
-							"UNDEAD RISING, LET'S TAKE OVER THIS FUCKING WOOOOORLD!\n" +
-							"THE ELECTRIC BOOGALOO, SIXTY NINE!!!\n" +
-							"NOICE!\n" +
-							"RETURN OF THE BOOGALOO\n" +
-							"ALWAYS THE BOOGALOO!!!!\n" +
-							"BOOGALOO AND FOREVER!\n" +
-							"FOREVER AND EVER BOOGALOO!!!!!!!\n" +
-							"IF YOU HAVEN'T SEEN THE BOOGALOO YOU HAVE NOW, BOOGALOO!\n" +
-							"OH SHIT, IT'S A MOTHER FUCKIN CRUSADE IN THIS BITCH!!!\n" +
-							"RAVE TIME!\n" +
-							"RAVE WITH THE DEAD!";
-						break;
-					case 8:
-						response = "I AM EVERWATCHING PANDA. PRAISE ME!\n" +
-							"HOLY FUCK IT'S EIGHT KINGS OF THE UNDEAD, QUEENS, AND PRINCESSES OF THE UNDEAD\n" +
-							"BY RIGHT OF THE GREAT PANDA!!!!!\n" +
-							"(and that old man too I guess)\n" +
-							"IT'S TIME TO COMMENCE OPERATION GOD NUMBERS OF EIGHT AS EIGHT!!!!!!!:\n" +
-							"UNDEAD RISING, LET'S TAKE OVER THIS FUCKING WOOOOORLD!\n" +
-							"THE ELECTRIC BOOGALOO, SIXTY NINE!!!\n" +
-							"NOICE!\n" +
-							"RETURN OF THE BOOGALOO\n" +
-							"ALWAYS THE BOOGALOO!!!!\n" +
-							"BOOGALOO AND FOREVER!\n" +
-							"FOREVER AND EVER BOOGALOO!!!!!!!\n" +
-							"IF YOU HAVEN'T SEEN THE BOOGALOO YOU HAVE NOW, BOOGALOO!\n" +
-							"OH SHIT, IT'S A MOTHER FUCKIN CRUSADE IN THIS BITCH!!!\n" +
-							"RAVE TIME!\n" +
-							"RAVE WITH THE DEAD!\n" +
-							"DEAD RAVE PARTY! BOOOGALOO RAVE!!!!\n" +
-							"J)#H%R_(Q#HRN_(Q#YVRN_Q(#YR0aw=038rya!!!!\n" +
-							"EIGHT AND EIGHT FOREVER AND EVER EIGHT \n" +
-							"EIGHT TIMES DEATH IS DEATH AND EIGHT, EIGHT, EIGHT!!!!\n" +
-							"ADFJ)QAHRQ)AWEYAW\n" +
-							"F)(QW#YN)WF\n" +
-							"HE COMES)@(H_$)#%$)+@#NU%+)@(#%H@#)(&%$FUCK MY LIFE)Y#_(%HQ@#+*$RHQ@# \n" +
-							"RAAAAAAAAAAAAAAAAAVE IN HEEEEEEEEEEEEEEEEERE!!!!!\n" +
-							"CURSE OF THE BLACK CRUSAAAAAAAAADE!!!!!\n" +
-							"FOR THE MOTHERLAND AND THE FATHERLAND!!!!!!\n" +
-							"EIGHT PARTS OF EIGHT!\n" +
-							"LET'S RAVE THIS BITCH EIGHT TIMES TONIGHT!";
-						break;
+			const server = client.guilds.cache.get(guildId);
+			if (server) {
+				const channel = server.channels.cache.get(logChannelId);
+				if (channel) {
+					channel.send(`${interaction.user.username} used 'kings'.\nOnline kings: ${membersWithRole.map(a => a.user.username).join(", ")}`); // Replace the message content with the desired message
+				} else {
+					console.log('Channel not found.');
 				}
-
-				await interaction.reply(response);
 			} else {
-				await interaction.reply('There are no Kings of Undead online. Cry, because it means this server is dying');
+				console.log('Server not found.');
 			}
+
+		if (onlineCount >= 1) {
+			let response;
+
+			switch (onlineCount) {
+				case 1:
+					response =
+						"There is only one king of undead online. Panda is sad.\n" +
+						"Remember, I'm watching you";
+					break;
+				case 2:
+					response =
+						"There two kings of undead online. Maybe they can try to breed more of them?\n" +
+						"Spoiler: Maybe sometime I'll write custom messages based on who's online \n" +
+						"and in place of the message upward you'll see encouragement for Cow to breed resident batcatgirl";
+					break;
+				case 3:
+					response = "Three kigns. PLACEHOLDER";
+					break;
+				case 4:
+					response = "I AM EVERWATCHING PANDA. PRAISE ME!\n" +
+						"FOUR KINGS OF THE UNDEAD ARE NOW ON!\n" +
+						"BY RIGHT OF THE GREAT PANDA!\n" +
+						"(and that old man too I guess)\n" +
+						"IT'S TIME TO COMMENCE OPERATION:\n" +
+						"PANDEMONIUM!\n" +
+						"THE ELECTRIC BOOGALOO, FOUR!\n" +
+						"RETURN OF THE BOOGALOO";
+					break;
+				case 5:
+					response = "I AM EVERWATCHING PANDA. PRAISE ME!\n" +
+						"FIVE KINGS OF THE UNDEAD ARE NOW ON!\n" +
+						"BY RIGHT OF THE GREAT PANDA!\n" +
+						"(and that old man too I guess)\n" +
+						"IT'S TIME TO COMMENCE OPERATION:\n" +
+						"UNDEAD RISING! FOR THE FIFTH TIME!!!\n" +
+						"THE ELECTRIC BOOGALOO, FIIIIIVE!\n" +
+						"RETURN OF THE BOOGALOO!\n" +
+						"OOGGY BOOGY'S BOOGALOO!\n" +
+						"THE BOOGALOO SPECIAL!\n" +
+						"THE NEXT BOOGALOO!!!!\n" +
+						"LURKERS UNITE FOR TEH BOOGALOO!!!!";
+					break;
+				case 6:
+					response = "I AM EVERWATCHING PANDA. PRAISE ME!\n" +
+						"SIX FUCKING KINGS OF THE UNDEAD;\n" +
+						"BY RIGHT OF THE GREAT PANDA!!!!!\n" +
+						"(and that old man too I guess)\n" +
+						"IT'S TIME TO COMMENCE OPERATION SIX SIX SIX!!!!!!!:\n" +
+						"UNDEAD RISING, LET'S TAKE OVER THIS FUCKING WOOOOORLD!\n" +
+						"THE ELECTRIC BOOGALOO, SIXTY NINE!!!\n" +
+						"NOICE!\n" +
+						"RETURN OF THE BOOGALOO\n" +
+						"ALWAYS THE BOOGALOO!!!!\n" +
+						"BOOGALOO AND FOREVER!\n" +
+						"FOREVER AND EVER BOOGALOO!!!!!!!\n" +
+						"IF YOU HAVEN'T SEEN THE BOOGALOO YOU HAVE NOW, BOOGALOO!\n" +
+						"OH SHIT, IT'S A MOTHER FUCKIN CRUSADE IN THIS BITCH!!!\n" +
+						"RAVE TIME!\n" +
+						"RAVE WITH THE DEAD!\n" +
+						"DEAD RAVE PARTY! BOOOGALOO RAVE!!!!\n" +
+						"J)#H%R_(Q#HRN_(Q#YVRN_Q(#YR0aw=038rya!!!!";
+					break;
+				case 7:
+					response = "I AM EVERWATCHING PANDA. PRAISE ME!\n" +
+						"SEVEN FUCKING SEVEN SEVEN KINGS OF THE UNDEAD;\n" +
+						"BY RIGHT OF THE GREAT PANDA!!!!!\n" +
+						"(and that old man too I guess)\n" +
+						"IT'S TIME TO COMMENCE OPERATION GOD NUMBERS OF SEVEN AND SEVEN!!!!!!!:\n" +
+						"UNDEAD RISING, LET'S TAKE OVER THIS FUCKING WOOOOORLD!\n" +
+						"THE ELECTRIC BOOGALOO, SIXTY NINE!!!\n" +
+						"NOICE!\n" +
+						"RETURN OF THE BOOGALOO\n" +
+						"ALWAYS THE BOOGALOO!!!!\n" +
+						"BOOGALOO AND FOREVER!\n" +
+						"FOREVER AND EVER BOOGALOO!!!!!!!\n" +
+						"IF YOU HAVEN'T SEEN THE BOOGALOO YOU HAVE NOW, BOOGALOO!\n" +
+						"OH SHIT, IT'S A MOTHER FUCKIN CRUSADE IN THIS BITCH!!!\n" +
+						"RAVE TIME!\n" +
+						"RAVE WITH THE DEAD!";
+					break;
+				case 8:
+					response = "I AM EVERWATCHING PANDA. PRAISE ME!\n" +
+						"HOLY FUCK IT'S EIGHT KINGS OF THE UNDEAD, QUEENS, AND PRINCESSES OF THE UNDEAD\n" +
+						"BY RIGHT OF THE GREAT PANDA!!!!!\n" +
+						"(and that old man too I guess)\n" +
+						"IT'S TIME TO COMMENCE OPERATION GOD NUMBERS OF EIGHT AS EIGHT!!!!!!!:\n" +
+						"UNDEAD RISING, LET'S TAKE OVER THIS FUCKING WOOOOORLD!\n" +
+						"THE ELECTRIC BOOGALOO, SIXTY NINE!!!\n" +
+						"NOICE!\n" +
+						"RETURN OF THE BOOGALOO\n" +
+						"ALWAYS THE BOOGALOO!!!!\n" +
+						"BOOGALOO AND FOREVER!\n" +
+						"FOREVER AND EVER BOOGALOO!!!!!!!\n" +
+						"IF YOU HAVEN'T SEEN THE BOOGALOO YOU HAVE NOW, BOOGALOO!\n" +
+						"OH SHIT, IT'S A MOTHER FUCKIN CRUSADE IN THIS BITCH!!!\n" +
+						"RAVE TIME!\n" +
+						"RAVE WITH THE DEAD!\n" +
+						"DEAD RAVE PARTY! BOOOGALOO RAVE!!!!\n" +
+						"J)#H%R_(Q#HRN_(Q#YVRN_Q(#YR0aw=038rya!!!!\n" +
+						"EIGHT AND EIGHT FOREVER AND EVER EIGHT \n" +
+						"EIGHT TIMES DEATH IS DEATH AND EIGHT, EIGHT, EIGHT!!!!\n" +
+						"ADFJ)QAHRQ)AWEYAW\n" +
+						"F)(QW#YN)WF\n" +
+						"HE COMES)@(H_$)#%$)+@#NU%+)@(#%H@#)(&%$FUCK MY LIFE)Y#_(%HQ@#+*$RHQ@# \n" +
+						"RAAAAAAAAAAAAAAAAAVE IN HEEEEEEEEEEEEEEEEERE!!!!!\n" +
+						"CURSE OF THE BLACK CRUSAAAAAAAAADE!!!!!\n" +
+						"FOR THE MOTHERLAND AND THE FATHERLAND!!!!!!\n" +
+						"EIGHT PARTS OF EIGHT!\n" +
+						"LET'S RAVE THIS BITCH EIGHT TIMES TONIGHT!";
+					break;
+			}
+
+			await interaction.reply(response);
 		} else {
-			await interaction.reply('Role not found.');
+			await interaction.reply('There are no Kings of Undead online. Cry, because it means this server is dying');
 		}
-	},
+	} else {
+		await interaction.reply('Role not found.');
+	}
+},
 };
